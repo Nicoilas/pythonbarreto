b5d14aa407f8376c792bc8c9fa185f66a6ab64e5
Nicolas <nb199144@gmail.com>
2023-04-24 17:32:10 -0500

    actividades de dia 24/04

---
+++ inicio/tarea/Actividad.py
@@ -0,0 +1,19 @@
+x =int(input("ingrese el numero: "))
+j =int(input("ingrese el numero: "))
+o =int(input("ingrese el numero: "))
+
+if j > x:
+
+ print ('j es mayor que: ',x)
+
+else:
+ 
+ print("x es menor que ",j)
+
+if o < x:
+ print("o es menor que",x)
+
+else:
+ print("x es mayor que",o)
+
+
