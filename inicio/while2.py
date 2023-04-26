a36be3393ec4a806d5541dcc241b7d2efcb13712
Nicolas <nb199144@gmail.com>
2023-04-26 17:37:20 -0500

    actividades y comentarios en los codigos del profesor

---
+++ inicio/while2.py
@@ -0,0 +1,13 @@
+num=int(print("ingrese un numero"))
+i=1
+
+while i<num:
+     if i%7==0:
+        print(f"{i} es multiplo de 7 ")     #se crea un ci¿ontador que es similar a la variable constante
+                                            #printe nos dice cuales seran los multiplos de 7 
+                                            
+     else:
+         print(i)
+         i+=1
+
+   
