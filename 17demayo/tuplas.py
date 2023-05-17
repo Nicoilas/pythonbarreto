fcb116c392800b67b267cff2c4fa5d96dd9a03cb
Nicolas <nb199144@gmail.com>
2023-05-17 17:08:45 -0500

    tuplasexplicacion

---
+++ 17demayo/tuplas.py
@@ -0,0 +1,19 @@
+import random
+
+tam=random.randint(5,15)
+t=()
+for i in range(tam):
+    n=random.randrange(100)
+    t+=(n,)
+    print(t)   
+
+t2=()
+t3=()
+t2[:7]
+t3[8:]
+print(t2)
+print(t3)
+print(type(t2))
+print(type(t3))  
+
+
