30f16933b38bb8cdb9e195e01e4c6d2c0c25a838
Nicolas <nb199144@gmail.com>
2023-05-29 17:05:31 -0500

    29/05

---
+++ extra/modulos/modulos.py
@@ -0,0 +1,10 @@
+import sys
+from sys import path
+path.append('..\\extra\\good\\best')
+
+import extra.iota
+import extra.good.alpha
+import extra.good.best.sigma
+print(extra.iota.FunI())
+print(extra.good.alpha.FunA())
+print(extra.good.best.sigma.FunS())
