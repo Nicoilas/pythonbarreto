bc32850a441e93bac6cc7db275315c492d033ac8
Nicolas <nb199144@gmail.com>
2023-07-31 16:17:22 -0500

    31 de julio

---
+++ 31/07/2023/ejercicio.py
@@ -0,0 +1,32 @@
+# a= int(input("ingrese numeros: "))
+# b= int(input("ingrese numeros: "))
+
+# def suma(a, b):
+#     return a + b
+
+# def resta(a, b):
+#     return a - b
+
+# def multiplicacion(a, b):
+#     return a * b
+
+# def division(a, b):
+#     if b != 0:
+#         return a / b
+#     else:
+#         return "Error: división por cero"
+
+# funciones = {
+#     "suma": suma,
+#     "resta": resta,
+#     "multiplicacion": multiplicacion,
+#     "division": division
+# }
+# resultado = funciones["suma"](a,b)
+# print(resultado)    
+
+# resultado = funciones["division"](a,b)
+# print(resultado)    
+
+# resultado = funciones["resta"](a,b)
+# print(resultado)    
