67324d0bf880241f0869fa590a6525172ab65624
Nicolas <nb199144@gmail.com>
2023-08-02 14:09:52 -0500

    actividad

--- 31/07/2023/ejercicio.py
+++ 31/07/2023/ejercicio.py
@@ -1,32 +1,41 @@
-# a= int(input("ingrese numeros: "))
-# b= int(input("ingrese numeros: "))
+import os
+def crear_archivo(nombre_archivo):
+    if os.path.exists(nombre_archivo):
+        print(f"El archivo '{nombre_archivo}' ya existe.")
+        with open(nombre_archivo, 'r') as file:
+            num_lineas = sum(1 for line in file)
+        print(f"El archivo contiene {num_lineas} líneas.")
+    else:
+        with open(nombre_archivo, 'w'):
+            print(f"El archivo '{nombre_archivo}' se ha creado.")
 
-# def suma(a, b):
-#     return a + b
+def guardar_datos_personales(nombre_archivo):
+    print("Ingrese sus datos personales.")
+    nombre = input("Nombre: ")
+    edad = input("Edad: ")
+    direccion = input("Dirección: ")
 
-# def resta(a, b):
-#     return a - b
+    with open(nombre_archivo, 'w') as file:
+        file.write(f"Nombre: {nombre}\n")
+        file.write(f"Edad: {edad}\n")
+        file.write(f"Dirección: {direccion}\n")
 
-# def multiplicacion(a, b):
-#     return a * b
+    print(f"Los datos personales se han guardado en el archivo '{nombre_archivo}'.")
 
-# def division(a, b):
-#     if b != 0:
-#         return a / b
-#     else:
-#         return "Error: división por cero"
+def contar_caracteres(nombre_archivo):
+    if not os.path.exists(nombre_archivo):
+        print(f"El archivo '{nombre_archivo}' no existe.")
+    else:
+        with open(nombre_archivo, 'r') as file:
+            contenido = file.read()
+            num_caracteres = len(contenido)
+        print(f"El archivo contiene {num_caracteres} caracteres.")
 
-# funciones = {
-#     "suma": suma,
-#     "resta": resta,
-#     "multiplicacion": multiplicacion,
-#     "division": division
-# }
-# resultado = funciones["suma"](a,b)
-# print(resultado)    
+if __name__ == "__main__":
+    archivo_nombre = "datos_personales.txt"
 
-# resultado = funciones["division"](a,b)
-# print(resultado)    
+    crear_archivo(archivo_nombre)
 
-# resultado = funciones["resta"](a,b)
-# print(resultado)    
+    guardar_datos_personales(archivo_nombre)
+  
+    contar_caracteres(archivo_nombre)
